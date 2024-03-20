c6b6c203df869b062b430e80b667950fe4937dfb
chichigan <louming123@hotmail.com>
2024-03-21 00:00:37 +0800

    Add Pizza model file

--- database/migrations/2024_03_20_092451_add_promotion_to_pizzas_table.php
+++ database/migrations/2024_03_20_092451_add_promotion_to_pizzas_table.php
@@ -26,7 +26,7 @@ class AddPromotionToPizzasTable extends Migration
     public function down()
     {
         Schema::table('pizzas', function (Blueprint $table) {
-            //
+            $table->dropColumn('promotion');
         });
     }
 }
